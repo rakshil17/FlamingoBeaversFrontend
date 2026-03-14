75b387fd3be28f5d76edc5e6eed446d534f93686
Pavith Preet Singh <pavithsingh@outlook.com>
2026-03-14 19:15:42 +1100

    hello

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Flamingo Beavers is a web application designed to help students manage their finances and track their expenses. With features like budgeting tools, expense tracking, and financial goal setting, Flamingo Beavers aims to empower students to take control of their financial future. Whether you're looking to save money, pay off debt, or simply stay on top of your finances, Flamingo Beavers has you covered.
+    </div>
+  )
+}
+
+export default Home
