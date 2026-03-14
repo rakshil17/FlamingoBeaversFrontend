11b577e797fe13577406aeef3a53c7d12936cae7
Pavith Preet Singh <pavithsingh@outlook.com>
2026-03-14 19:58:18 +1100

    Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
+import { FaSearch, FaUniversity, FaBook, FaArrowDown } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div>
-      Flamingo Beavers is a web application designed to help students manage their finances and track their expenses. With features like budgeting tools, expense tracking, and financial goal setting, Flamingo Beavers aims to empower students to take control of their financial future. Whether you're looking to save money, pay off debt, or simply stay on top of your finances, Flamingo Beavers has you covered.
-    </div>
-  )
-}
+    <div className="hero">
 
-export default Home
+      <h1>
+        Explore and compare institutions and study areas based on
+        real life student experiences
+      </h1>
+
+      <div className="search-box">
+        <FaSearch className="search-icon" />
+        <input
+          type="text"
+          placeholder="Search institution or study area"
+        />
+      </div>
+
+      <div className="browse-links">
+        <div className="browse-item">
+          <FaUniversity className="browse-icon"/>
+          Browse all institutions
+        </div>
+
+        <div className="browse-item">
+          <FaBook className="browse-icon"/>
+          Browse all study areas
+        </div>
+      </div>
+
+      <p className="qilt-text">
+        QILT survey responses allow you to explore the experiences of
+        2 million students and graduates of different institutions
+        and study areas to help inform your study choices.
+      </p>
+
+      <div className="scroll-btn">
+        <FaArrowDown />
+      </div>
+
+    </div>
+  );
+};
+
+export default Home;
