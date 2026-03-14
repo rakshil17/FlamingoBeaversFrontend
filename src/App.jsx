141a0646b54af8ea6a91e500a227bbc90dca5fc7
sln2366 <sln2366ghen@gmail.com>
2026-03-14 19:18:35 +1100

    ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,9 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Flamingo Beavers</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            UNSW <code>help</code> and save to test <code>HMR</code>
           </p>
         </div>
         <button
